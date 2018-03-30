c7e82e19de46178e48599717c0696ce875f3d705
John Smith <hotzel-schm@online.de>
2018-03-30 17:18:44 +0200

    speedgit eingeführt

---
+++ docker-entrypoint-gittest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#set -e
+#
+#if [ "${1:0:1}" = '-' ]; then
+#	set -- speedtest-cli "$@"
+#fi
+#
+#exec "$@"
+
+#export http_proxy=http://10.50.8.52:8080
+#export https_proxy=http://10.50.8.52:8080
+
+git clone https://github.com/gshipley/openshift3mlbparks mlbparks
+cd mlbparks/
+mvn clean package
